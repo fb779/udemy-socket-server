4c6ab80e7544d555f7ed51a45231b90417e567f3
Fabian Forero <fabianforero779@gmail.com>
2020-08-09 06:10:46 -0400

    Instalacion y configuracion del socket
    Se realiza la instalacion y configuracion basica del socket, verificacion de funcionamiento conjunto de la api y el socket

--- src/class/server.ts
+++ src/class/server.ts
@@ -1,24 +1,29 @@
+import http from 'http';
 import express, { Router } from 'express';
 import { SERVER_PORT } from '../global/environmet';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
-
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
+import socketIO from 'socket.io';
 
 export default class Server {
+  private httpServer: http.Server;
   public app: express.Application;
   public port: Number;
+  public io: socketIO.Server;
 
   constructor() {
     this.app = express();
     this.port = SERVER_PORT;
     this.addPlugins();
+
+    this.httpServer = new http.Server(this.app);
+    this.io = socketIO(this.httpServer);
   }
 
   start(callback: any) {
-    this.app.listen(this.port, callback);
+    // this.app.listen(this.port, callback);
+    this.httpServer.listen(this.port, callback);
   }
 
   addPlugins() {
@@ -30,5 +35,13 @@ export default class Server {
 
   addRouter(router: Router) {
     this.app.use(router);
+  }
+
+  private listenSocket() {
+    console.log('escuchando conecciones - socket');
+
+    this.io.on('connection', (client) => {
+      console.log('nueva coneccion al socket');
+    });
   }
 }
